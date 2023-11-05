fa51d9a03e4adf1448fa8dd07d37464e1b9b104a
lanangksma <kusumalanang1@gmail.com>
2023-11-06 06:18:09 +0700

    15. User Registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Validator;
 
 class RegisterController extends Controller
 {
@@ -13,4 +15,20 @@ class RegisterController extends Controller
            "active" => "register"
        ]);
    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|min:5|max:254',
+            'username' => ['required', 'min:5', 'max:254', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:254'
+        ]);
+
+        $validatedData['password'] = bcrypt($validatedData['password']);
+        User::create($validatedData);
+
+//        $request->session()->flash('success', 'Registration success! Please login.');
+        return redirect('/login')->with('success', 'Registration success! Please login.');
+    }
 }

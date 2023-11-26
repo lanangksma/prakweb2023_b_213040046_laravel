37752f485945018cfce4738c96be8332c2201c8c
lanangksma <kusumalanang1@gmail.com>
2023-11-26 19:27:27 +0700

    23. Authorization

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -18,6 +18,22 @@
                 </li>
             </ul>
 
+            @can('admin')
+                <hr class="my-2">
+                <h6 class="sidebar-heading d-flex justify-content-between align-item-center px-3 mt-4 mb-1 text-muted">
+                    <span>ADMINISTRATOR</span>
+                </h6>
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard/categories*') ? 'active' : '' }}"
+                            href="/dashboard/categories">
+                            <i data-feather="grid"></i>
+                            Post Categories
+                        </a>
+                    </li>
+                </ul>
+            @endcan
+
             <hr class="my-3">
 
             <ul class="nav flex-column mb-auto">
--- routes/web.php
+++ routes/web.php
@@ -61,3 +61,5 @@ Route::get('/dashboard', function () {
 
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class) -> middleware('auth');
+
+Route::resource('/dashboard/categories', \App\Http\Controllers\AdminCategoryController::class) -> middleware('admin') -> except('show');
